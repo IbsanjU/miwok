e48015017249d25ba1867ecc982c8ce4a9c423e7
ibsanju <bharath2668@gmail.com>
2020-07-02 21:52:10 +0530

    onclick and Array list + Custom adapter

--- app/src/main/java/com/ibsanju/miwok/MainActivity.java
+++ app/src/main/java/com/ibsanju/miwok/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public
 class MainActivity extends AppCompatActivity {
@@ -15,23 +16,46 @@ class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // find the view that shows thw numbers category
+        TextView numbers = findViewById(R.id.numbers);
+
+        // Set a clicklistener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public
+            void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
     }
-    public void openNumbersList(View v) {
+
+    public
+    void openNumbersList(View v) {
         // TODO: Write your code here!
-        Intent i = new Intent(MainActivity.this, NumbersActivity.class);
-        startActivity(i);
+        // Create a new intent to open the {@link NumbersActivity}
+//        Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+        // Start new Activity
+//        startActivity(i);
     }
-    public void openColorsList(View v) {
+
+    public
+    void openColorsList(View v) {
         // TODO: Write your code here!
         Intent i = new Intent(MainActivity.this, ColorsActivity.class);
         startActivity(i);
     }
-    public void openFamilyList(View v) {
+
+    public
+    void openFamilyList(View v) {
         // TODO: Write your code here!
         Intent i = new Intent(MainActivity.this, FamilyActivity.class);
         startActivity(i);
     }
-    public void openPhrasesList(View v) {
+
+    public
+    void openPhrasesList(View v) {
         // TODO: Write your code here!
         Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
         startActivity(i);
--- app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
+++ app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
 
 public
 class NumbersActivity extends AppCompatActivity {
@@ -13,6 +21,70 @@ class NumbersActivity extends AppCompatActivity {
     void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        Log.v("NumbersActivity: ", " was here");
+
+//        ArrayList<String> words = new ArrayList<String>();
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+//        words.add("one");
+        Word W = new Word("one","lutti");
+        words.add(W);
+
+
+
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+//    LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+//        // Create a variable to keep track of the current index position
+//        int index = 0;
+//
+//        // Keep looping until we've reached the end of the list (which means keep looping
+//        // as long as the current index position is less than the length of the list)
+//        while(index<words.size()) {
+//            // Create a new TextView
+//            TextView wordView = new TextView(this);
+//
+//            // Set the text to be word at the current index
+//            wordView.setText(words.get(index));
+//
+//            // Add this TextView as another child to the root view of this layout
+//            rootView.addView(wordView);
+//
+//            // Increment the index variable by 1
+//            index++;
+//        }
+
+//        for (int index = 0; index< words.size(); index++){
+//            // Create a new TextView
+//            TextView wordView = new TextView(this);
+//
+//            // Set the text to be word at the current index
+//            wordView.fsetText(words.get(index));
+//
+//            // Add this TextView as another child to the root view of this layout
+//            rootView.addView(wordView);
+//        }
+
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+        ListView listView = (ListView) findViewById(R.id.list);
+//        GridView listView = (GridView) findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
+
+//        Here is a list of common Type Parameters:
+//
+//        E - Element (used extensively by the Java Collections Framework)
+//        K - Key
+//        N - Number
+//        T - Type
+//        V - Value
+//        S,U,V etc. - 2nd, 3rd, 4th types (when there is more than one parameter)
     }
 }
