25f0e26d6b5a46f65bc469ec92bb842eefc5bf8f
ibsanju <bharath2668@gmail.com>
2020-07-04 21:49:25 +0530

    audiofucus AudioManager
    playback on conditions on external events

--- app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
+++ app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.ibsanju.miwok;
 import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +22,12 @@ class NumbersActivity extends AppCompatActivity {
 	 * This listener gets triggered when the {@link MediaPlayer} has completed
 	 * playing the audio file.
 	 */
+	private MediaPlayer mMediaPlayer;
+
+	/** Handles audio focus when playing a sound file */
+	private AudioManager mAudioManager;
+
+
 	private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
 		@Override
 		public void onCompletion(MediaPlayer mediaPlayer) {
@@ -28,13 +36,43 @@ class NumbersActivity extends AppCompatActivity {
 		}
 	};
 
-	private MediaPlayer mMediaPlayer;
+	/**
+	 * This listener gets triggered whenever the audio focus changes
+	 * (i.e., we gain or lose audio focus because of another app or device).
+	 */
+	private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+		@Override
+		public void onAudioFocusChange(int focusChange) {
+			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+					focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+				// The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+				// short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+				// our app is allowed to continue playing sound but at a lower volume. We'll treat
+				// both cases the same way because our app is playing short sound files.
+
+				// Pause playback and reset player to the start of the file. That way, we can
+				// play the word from the beginning when we resume playback.
+				mMediaPlayer.pause();
+				mMediaPlayer.seekTo(0);
+			} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+				// The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+				mMediaPlayer.start();
+			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+				// The AUDIOFOCUS_LOSS case means we've lost audio focus and
+				// Stop playback and clean up resources
+				releaseMediaPlayer();
+			}
+		}
+	};
 
 	@Override
 	protected
 	void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.word_list);
+
+		// Create and setup the {@link AudioManager} to request audio focus
+		mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
 //        ArrayList<String> words = new ArrayList<String>();
 		// Create a list of words
@@ -52,37 +90,37 @@ class NumbersActivity extends AppCompatActivity {
 		words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
 		words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
 
-//    LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+/*
+    LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        // Create a variable to keep track of the current index position
+        int index = 0;
 
-//        // Create a variable to keep track of the current index position
-//        int index = 0;
-//
-//        // Keep looping until we've reached the end of the list (which means keep looping
-//        // as long as the current index position is less than the length of the list)
-//        while(index<words.size()) {
-//            // Create a new TextView
-//            TextView wordView = new TextView(this);
-//
-//            // Set the text to be word at the current index
-//            wordView.setText(words.get(index));
-//
-//            // Add this TextView as another child to the root view of this layout
-//            rootView.addView(wordView);
-//
-//            // Increment the index variable by 1
-//            index++;
-//        }
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list)
+        while(index<words.size()) {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
 
-//        for (int index = 0; index< words.size(); index++){
-//            // Create a new TextView
-//            TextView wordView = new TextView(this);
-//
-//            // Set the text to be word at the current index
-//            wordView.fsetText(words.get(index));
-//
-//            // Add this TextView as another child to the root view of this layout
-//            rootView.addView(wordView);
-//        }
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+
+            // Increment the index variable by 1
+            index++;
+        }
+        for (int index = 0; index< words.size(); index++){
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.fsetText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
+*/
 
 		WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
 		// Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
@@ -107,6 +145,14 @@ class NumbersActivity extends AppCompatActivity {
 //					Log.d(TAG, "onItemClick: CurrentWord: " + word);
 					Log.d(TAG, "CurrentWord: " + word.toString());
 
+
+				// Request audio focus so in order to play the audio file. The app needs to play a
+				// short audio file, so we will request audio focus with a short amount of time
+				// with AUDIOFOCUS_GAIN_TRANSIENT.
+				int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+				if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+					// We have audio focus now.
 					// Create and setup the {@link MediaPlayer} for the audio resource associated
 					// with the current word
 					mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
@@ -114,10 +160,12 @@ class NumbersActivity extends AppCompatActivity {
 					// Start the audio file
 					mMediaPlayer.start();
 
+					// Setup a listener on the media player, so that we can stop and release the
+					// media player once the sound has finished playing.
+					mMediaPlayer.setOnCompletionListener(mCompletionListener);
+				}
 
-				// Setup a listener on the media player, so that we can stop and release the
-				// media player once the sound has finished playing.
-				mMediaPlayer.setOnCompletionListener(mCompletionListener);
+
 				}
 			});
 //        Here is a list of common Type Parameters:
@@ -144,6 +192,10 @@ class NumbersActivity extends AppCompatActivity {
 			// setting the media player to null is an easy way to tell that the media player
 			// is not configured to play an audio file at the moment.
 			mMediaPlayer = null;
+
+			// Regardless of whether or not we were granted audio focus, abandon it. This also
+			// unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+			mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
 		}
 	}
 
